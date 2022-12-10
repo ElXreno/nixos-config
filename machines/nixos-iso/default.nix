ce61293bb5818bab638952131eef62824f6bd5e4
ElXreno <elxreno@gmail.com>
2022-12-10 22:39:34 +0300

    nixos-iso: Use zstd with low compression level

--- machines/nixos-iso/default.nix
+++ machines/nixos-iso/default.nix
@@ -5,4 +5,6 @@
     "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5-new-kernel.nix"
     inputs.self.nixosRoles.iso
   ];
+
+  isoImage.squashfsCompression = "zstd -Xcompression-level 4";
 }
