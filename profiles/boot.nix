4a8616be5383260c7d05e6eb8ef2c1fc3d7b17cb
ElXreno <elxreno@gmail.com>
2022-12-17 20:13:54 +0300

    boot: Update sysctl parameters

--- profiles/boot.nix
+++ profiles/boot.nix
@@ -46,16 +46,21 @@
     ];
     kernel.sysctl = lib.mkMerge [
       {
+        # Network
         "net.core.default_qdisc" = "fq";
         "net.core.netdev_max_backlog" = "16384";
         "net.ipv4.tcp_congestion_control" = "bbr";
-        "net.ipv4.tcp_fastopen" = 3;
+        "net.ipv4.tcp_ecn" = 1;
         "net.ipv4.tcp_keepalive_intvl" = 10;
         "net.ipv4.tcp_keepalive_probes" = 6;
-        "net.ipv4.tcp_keepalive_time" = 60;
+        "net.ipv4.tcp_keepalive_time" = 30;
+        "net.ipv4.tcp_low_latency" = 1;
         "net.ipv4.tcp_mtu_probing" = 1;
         "net.ipv4.tcp_notsent_lowat" = "16384";
         "net.ipv4.tcp_slow_start_after_idle" = 0;
+        "net.ipv4.tcp_tw_reuse" = 1;
+
+        # Memory
         "vm.oom_kill_allocating_task" = 1;
         "vm.overcommit_memory" = 2;
         "vm.overcommit_ratio" = 400;
@@ -76,15 +81,17 @@
       (
         lib.mkIf config.deviceSpecific.isLaptop
           {
+            # Network
             "net.ipv4.ip_default_ttl" = 65;
             "net.ipv6.conf.all.hop_limit" = 65;
             "net.ipv6.conf.default.hop_limit" = 65;
             "net.ipv6.conf.lo.hop_limit" = 65;
             "net.ipv6.conf.wlp1s0.hop_limit" = 65;
 
-            "net.ipv4.tcp_congestion_control" = lib.mkForce "bbr2";
-
-            "vm.page-cluster" = 6;
+            # Memory
+            "vm.extfrag_threshold" = 0;
+            "vm.vfs_cache_pressure" = 3000;
+            "vm.page-cluster" = 2;
           }
       )
     ];
