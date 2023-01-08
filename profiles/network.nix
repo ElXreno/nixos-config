e6f8fbf4905031dd415c34b998f3f696a8e05c0c
ElXreno <elxreno@gmail.com>
2023-01-09 02:55:34 +0300

    network: Finally drop iwd

--- profiles/network.nix
+++ profiles/network.nix
@@ -5,23 +5,9 @@
     networkmanager = {
       enable = lib.mkIf (!config.deviceSpecific.isServer || config.device == "Noxer-Server") true;
       wifi = {
-        backend = "iwd";
         powersave = false;
       };
     };
     useDHCP = lib.mkIf (!config.deviceSpecific.isServer) false;
-    # fix iwd race by disabling iface management
-    wireless.iwd.settings = {
-      General = {
-        UseDefaultInterface = true;
-        AddressRandomization = "network";
-        ManagementFrameProtection = 1;
-        ControlPortOverNL80211 = true;
-        DisableANQP = true;
-      };
-      Network = {
-        EnableIPv6 = true;
-      };
-    };
   };
 }
