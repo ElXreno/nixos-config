9da43382cb4a044150b2560099f57b43c372ad08
ElXreno <elxreno@gmail.com>
2023-01-27 13:18:30 +0300

    ISO: Provide rtl8723b specific files & include virtual keyboard
    This allows me to boot NixOS on Prestigio Visconte 2 with working Bluetooth (BT Keyboard and touchpad works fine).

--- machines/nixos-iso/default.nix
+++ machines/nixos-iso/default.nix
@@ -1,5 +1,7 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
+let rtl8723b-firmware = pkgs.callPackage ./rtl8723b-firmware.nix { };
+in
 {
   imports = [
     "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5-new-kernel.nix"
@@ -7,4 +9,17 @@
   ];
 
   isoImage.squashfsCompression = "zstd -Xcompression-level 4";
+
+  environment.systemPackages = with pkgs; [
+    maliit-keyboard # Virtual keyboard
+  ];
+
+  hardware.firmware = [ rtl8723b-firmware ];
+
+  hardware.bluetooth = {
+    enable = true;
+    # For battery provider, bluezFull is just an alias for bluez
+    package = pkgs.bluez5-experimental;
+    settings.General.Experimental = true;
+  };
 }
