fe200a9f8a5ff44afdd4820817f24ac659f52841
ElXreno <elxreno@gmail.com>
2023-02-02 02:41:40 +0300

    Revert "hardware: opengl: Override mesa for INFINITY"
    This reverts commit e13f7aa73d07319701b819b7c09859032beedcec.

--- profiles/hardware.nix
+++ profiles/hardware.nix
@@ -38,24 +38,15 @@
       intel.updateMicrocode = lib.mkIf (config.device == "Fujitsu-AH531-Laptop") true;
     };
 
-    opengl =
-      let
-        mesa-overrides = {
-          galliumDrivers = [ "zink" "radeonsi" "swrast" "virgl" ];
-          vulkanDrivers = [ "amd" "swrast" "virtio-experimental" ];
-        };
-      in
-      {
-        enable = true;
-        driSupport32Bit = true;
-        package = lib.mkIf (config.device == "INFINITY") (pkgs.mesa.override mesa-overrides).drivers;
-        package32 = lib.mkIf (config.device == "INFINITY") (pkgs.driversi686Linux.mesa.override mesa-overrides).drivers;
-        extraPackages = with pkgs; lib.mkIf (config.device == "INFINITY") [ amdvlk ];
-        extraPackages32 = with pkgs.driversi686Linux; lib.mkIf (config.device == "INFINITY") [ amdvlk ];
+    opengl = {
+      enable = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; lib.mkIf (config.device == "INFINITY") [ amdvlk ];
+      extraPackages32 = with pkgs.driversi686Linux; lib.mkIf (config.device == "INFINITY") [ amdvlk ];
 
-        # Work-around for LibreOffice to get OpenCL work
-        setLdLibraryPath = true;
-      };
+      # Work-around for LibreOffice to get OpenCL work
+      setLdLibraryPath = true;
+    };
 
     bluetooth = {
       # TODO: Fix state after persist
