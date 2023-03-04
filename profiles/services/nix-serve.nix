65d6d3629ba36a05c29176548acef6ab2336b56b
ElXreno <elxreno@gmail.com>
2023-03-04 17:32:37 +0300

    nix-serve: Use default nix version

--- profiles/services/nix-serve.nix
+++ profiles/services/nix-serve.nix
@@ -1,4 +1,4 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, ... }:
 
 {
   imports = [ inputs.nix-serve-ng.nixosModules.default ];
@@ -9,10 +9,6 @@
 
   services.nix-serve = {
     enable = true;
-    # https://github.com/NixOS/nix/issues/7704
-    package = pkgs.nix-serve.override {
-      nix = pkgs.nixVersions.nix_2_12;
-    };
     secretKeyFile = config.sops.secrets."nix_store_cache".path;
   };
 }
