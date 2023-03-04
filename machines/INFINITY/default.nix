111c7ee3678138c73dac285b1843c70e94705915
ElXreno <elxreno@gmail.com>
2023-03-04 17:33:23 +0300

    INFINITY: Disable zfs
    OpenZFS doesn't support kernel 6.2

--- machines/INFINITY/default.nix
+++ machines/INFINITY/default.nix
@@ -34,7 +34,7 @@ in
     inputs.self.nixosProfiles.kde
     inputs.self.nixosProfiles.gamemode
     inputs.self.nixosProfiles.virtualisation
-    inputs.self.nixosProfiles.zfs
+    # inputs.self.nixosProfiles.zfs
     inputs.self.nixosProfiles.nix-serve
     # Lazy to configure everything from zero
     # inputs.self.nixosProfiles.sway
