ce806c6b7549da149fa5de54153dab7ffa160178
ElXreno <elxreno@gmail.com>
2023-04-18 13:03:57 +0300

    nix: Use INFINITY if possible

--- profiles/nix.nix
+++ profiles/nix.nix
@@ -12,11 +12,12 @@
         "https://elxreno.cachix.org"
         "https://r-ryantm.cachix.org"
         "https://nix-community.cachix.org"
-      ];
+      ] ++ lib.optional (config.device != "INFINITY" && config.services.tailscale.enable) "http://100.93.5.12:5000";
       trusted-public-keys = [
         "elxreno.cachix.org-1:ozSPSY5S3/TpbcXi+/DdtSj1JlK3CPz3G+F92yRBXDQ="
         "r-ryantm.cachix.org-1:gkUbLkouDAyvBdpBX0JOdIiD2/DP1ldF3Z3Y6Gqcc4c="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "infinity.tail1f457.ts.net:tRoBCI6Slhk8+6DmvhoOQbj2yukhN+5TjchhEFEbWcI="
       ];
 
       # auto-optimise-store = config.deviceSpecific.isLaptop;
