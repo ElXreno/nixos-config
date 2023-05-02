4b8c29eb38f08551c53f7a2367e7030416efc23b
ElXreno <elxreno@gmail.com>
2023-05-02 20:29:19 +0300

    htop: Show Network I/O

--- profiles/home/programs/htop.nix
+++ profiles/home/programs/htop.nix
@@ -19,8 +19,8 @@ in
         header_layout = "two_50_50";
         column_meters_0 = [ "LeftCPUs2" "Memory" "Swap" "Zram" ] ++ lib.optionals zfsEnabled [ "ZFSARC" "ZFSCARC" ];
         column_meter_modes_0 = [ 1 1 1 1 ] ++ lib.optionals zfsEnabled [ 2 2 ];
-        column_meters_1 = [ "RightCPUs2" "Tasks" "LoadAverage" "Uptime" "DiskIO" ];
-        column_meter_modes_1 = [ 1 2 2 2 2 ];
+        column_meters_1 = [ "RightCPUs2" "Tasks" "LoadAverage" "Uptime" "DiskIO" "NetworkIO" ];
+        column_meter_modes_1 = [ 1 2 2 2 2 2 ];
         # TODO: Write custom config generator
         # screen_tabs = 1;
         # "screen:I/O" = [ "PID" "USER" "IO_PRIORITY" "IO_RATE" "IO_READ_RATE" "IO_WRITE_RATE" "COMM" ];
