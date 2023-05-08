afe972c894472462219bf1ddfbf732b5d7c72be8
ElXreno <elxreno@gmail.com>
2023-05-08 21:49:31 +0300

    boot: Force fix up TTL on laptop

--- profiles/boot.nix
+++ profiles/boot.nix
@@ -98,4 +98,14 @@ in
       tmpfsSize = lib.mkIf config.zramSwap.enable "180%";
     };
   };
+
+  systemd.services.ttl-fixup = lib.mkIf isLaptop {
+      description = "Force fix up TTL";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.iptables}/bin/iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65";
+      };
+   };
 }
