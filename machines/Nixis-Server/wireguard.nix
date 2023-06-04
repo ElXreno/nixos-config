eb22558490d5e48d108e226682bb2136bb9bc28a
ElXreno <elxreno@gmail.com>
2023-06-04 22:22:47 +0300

    Nixis-Server: add another peer

--- machines/Nixis-Server/wireguard.nix
+++ machines/Nixis-Server/wireguard.nix
@@ -20,6 +20,10 @@
         publicKey = "lIwMys+xNAS1qNm+rYjErNkijcWQ01XazlH2Rl7uHl4=";
         allowedIPs = [ "10.0.0.2/32" ];
       }
+      {
+        publicKey = "JcOYsdD3A0r7X8kYKNrj+Z/ZD7/9M693rQiRyP39iy8=";
+        allowedIPs = [ "10.0.0.3/32" ];
+      }
     ];
   };
 
