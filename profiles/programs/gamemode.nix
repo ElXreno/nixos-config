b6177a131c7bb5c8d714b985ea90952f52c93b28
ElXreno <elxreno@gmail.com>
2023-06-27 13:21:58 +0300

    gamemode: Don't touch my gpu

--- profiles/programs/gamemode.nix
+++ profiles/programs/gamemode.nix
@@ -8,12 +8,6 @@
         renice = 10;
       };
 
-      gpu = lib.mkIf (config.device == "INFINITY") {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
-
       custom = {
         start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
         end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
