614603ae66f0390b5f10aa48c2ea26e96eafffed
ElXreno <elxreno@gmail.com>
2023-07-22 07:34:07 +0300

    graalvm-ee: 22.3.1 -> 22.3.3

--- sources/graalvm-ee-sources.nix
+++ sources/graalvm-ee-sources.nix
@@ -1,14 +1,17 @@
+# https://www.oracle.com/downloads/graalvm-downloads.html
+# GraalVM Enterprise 22
+
 {
   "native-image-installable-svm" = {
     "17-linux-amd64" = {
-      sha256 = "98cb72e47400cba96daf19acd6780af4b975738e6c180793e6ea82e863633b31";
-      url = "https://download.oracle.com/otn/utilities_drivers/oracle-labs/native-image-installable-svm-svmee-java17-linux-amd64-22.3.1.jar";
+      sha256 = "9N0IEJU3tO+Sp5ozR+iFFuSsYYfzO/H4Ug2bq/9Vzg4=";
+      url = "https://download.oracle.com/otn/utilities_drivers/oracle-labs/native-image-installable-svm-svmee-java17-linux-amd64-22.3.3.jar";
     };
   };
   "graalvm-ee" = {
     "17-linux-amd64" = {
-      sha256 = "67b71877727d7771dd1da652a31099c8f12b9050b541e47f11a382997c042edf";
-      url = "https://download.oracle.com/otn/utilities_drivers/oracle-labs/graalvm-ee-java17-linux-amd64-22.3.1.tar.gz";
+      sha256 = "z/w6NG9LGkd9KidBp71oVEnP0TyYr2/IBVWQfywbsLU=";
+      url = "https://download.oracle.com/otn/utilities_drivers/oracle-labs/graalvm-ee-java17-linux-amd64-22.3.3.tar.gz";
     };
   };
 }
