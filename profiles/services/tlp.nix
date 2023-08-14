69fea560a5e545fb5856e2a809c2401d48d36b49
ElXreno <elxreno@gmail.com>
2023-08-14 20:51:51 +0300

    tlp: Disable battery thresholds

--- profiles/services/tlp.nix
+++ profiles/services/tlp.nix
@@ -3,8 +3,6 @@
     tlp = {
       enable = true;
       settings = {
-        START_CHARGE_THRESH_BAT0 = 80;
-        STOP_CHARGE_THRESH_BAT0 = 90;
         CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
         PCIE_ASPM_ON_BAT = "powersupersave";
