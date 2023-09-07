839b40693ff4c85fbf338ac22d5235c7f8fa5add
ElXreno <elxreno@gmail.com>
2023-09-07 19:01:21 +0300

    Revert "INFINITY: Drop steam"
    This reverts commit a993787d30294cd8bd0c84350574d2cfbb4eca0b.

--- machines/INFINITY/default.nix
+++ machines/INFINITY/default.nix
@@ -53,6 +53,7 @@
   networking.firewall.allowedUDPPorts = [ 36645 ];
 
   programs.nix-ld.enable = true;
+  programs.steam.enable = true;
   programs.k3b.enable = true;
   programs.noisetorch.enable = true;
   services.tailscale.enable = true;
