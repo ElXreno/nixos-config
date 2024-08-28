b052212f5c00dae492eac8b60792400837135e23
ElXreno <elxreno@gmail.com>
2024-08-28 15:02:31 +0300

    nix: Clean up

--- profiles/nix.nix
+++ profiles/nix.nix
@@ -1,6 +1,13 @@
 { config, inputs, lib, ... }: {
   nix = {
     settings = {
+
+      auto-optimise-store = config.deviceSpecific.isServer;
+
+      builders-use-substitutes = true;
+
+      experimental-features = [ "nix-command" "flakes" ];
+
       extra-sandbox-paths = [
         (lib.mkIf config.programs.ccache.enable
           "${config.programs.ccache.cacheDir}")
@@ -9,17 +16,10 @@
       trusted-users = [ "elxreno" ]
         ++ lib.optional config.services.hydra.enable "hydra";
 
-      substituters = lib.mkForce ([
-        #"https://aseipp-nix-cache.global.ssl.fastly.net"
-        "https://cache.nixos.org"
-        "https://elxreno.cachix.org"
-      ]); # ++ lib.optional (config.device != "INFINITY" && config.services.tailscale.enable) "https://infinity.tail1f457.ts.net");
-      trusted-public-keys = [
-        "elxreno.cachix.org-1:ozSPSY5S3/TpbcXi+/DdtSj1JlK3CPz3G+F92yRBXDQ="
-        #        "infinity.tail1f457.ts.net:tRoBCI6Slhk8+6DmvhoOQbj2yukhN+5TjchhEFEbWcI="
-      ];
-
-      auto-optimise-store = config.deviceSpecific.isServer;
+      substituters =
+        lib.mkForce [ "https://cache.nixos.org" "https://elxreno.cachix.org" ];
+      trusted-public-keys =
+        [ "elxreno.cachix.org-1:ozSPSY5S3/TpbcXi+/DdtSj1JlK3CPz3G+F92yRBXDQ=" ];
     };
 
     daemonCPUSchedPolicy = "batch";
@@ -29,11 +29,6 @@
     registry.nixpkgs.flake = inputs.nixpkgs;
 
     nixPath = lib.mkForce [ "nixpkgs=${inputs.nixpkgs}" ];
-
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      builders-use-substitutes = true
-    '';
 
     gc = lib.mkIf config.deviceSpecific.isServer {
       automatic = true;
