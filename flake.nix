3a44fb669022c1ae5805b625549a40924e1f4518
ElXreno <elxreno@gmail.com>
2024-09-02 19:17:45 +0300

    Revert "flake.nix: Switch nixpkgs to master branch"
    My hydra x86_64-linux builder is not capable for kernel builds,
    and anyway it's done just for testing.
    
    This reverts commit acd68149a61d57fe55187bf0c8ef49ed9caa6c37.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A big collection of crappy hacks";
 
   inputs = {
-    # nixpkgs.url = "github:ElXreno/nixpkgs/nixos-unstable-cust";
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:ElXreno/nixpkgs/nixos-unstable-cust";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
