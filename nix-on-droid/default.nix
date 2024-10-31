3583d772cce21e8d1a046c8ff46caf4738452ef5
ElXreno <elxreno@gmail.com>
2024-10-31 11:58:23 +0300

    Revert "nix-on-droid: Substitute on flamingo"
    This reverts commit b5f75ae9d414cf3cf08bc6c5df8869ff607bfb8e.

--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -119,8 +119,6 @@ in {
     # https://github.com/nix-community/nix-on-droid/pull/270
     extraOptions = ''
       experimental-features = nix-command flakes
-      extra-substituters http://100.81.15.62:8080/elxreno
-      extra-trusted-public-keys elxreno:tZ38Gs0Wmc5fpulInZeahMwgFyFzFBRMTxFlx4LwRVE=
     '';
 
     registry.nixpkgs.flake = inputs.nixpkgs;
