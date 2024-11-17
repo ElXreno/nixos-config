6c4fd359c7cf2f1af1a64239f9feed5c031efed4
ElXreno <elxreno@gmail.com>
2024-11-17 13:53:38 +0300

    INFINITY: Drop pstate module (amd-pstate-epp now enabled by default)

--- machines/INFINITY/default.nix
+++ machines/INFINITY/default.nix
@@ -6,7 +6,6 @@
     ./wireguard.nix
     inputs.disko.nixosModules.disko
     inputs.nixos-hardware.nixosModules.common-cpu-amd
-    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-gpu-amd
     inputs.nixos-hardware.nixosModules.common-pc-laptop
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
