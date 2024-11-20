2fed74fd9534ff6259fc386768c6ea9b4b6d6cb7
ElXreno <elxreno@gmail.com>
2024-11-20 14:25:29 +0300

    gpg-agent: Add missing pinentry package

--- profiles/home/services/gpg-agent.nix
+++ profiles/home/services/gpg-agent.nix
@@ -1,7 +1,8 @@
-{
+{ pkgs, ... }: {
   home-manager.users.elxreno.services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
     defaultCacheTtl = 300;
+    pinentryPackage = pkgs.pinentry-qt;
   };
 }
