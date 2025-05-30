3d73f6d651c18e12d0e3d988d3d07cc0ff449848
ElXreno <elxreno@gmail.com>
2025-05-30 22:36:20 +0300

    tailscale: Work together with dnscrypt-proxy2

--- profiles/services/tailscale.nix
+++ profiles/services/tailscale.nix
@@ -1,10 +1,31 @@
 { config, lib, ... }:
+let
+  ts = config.services.tailscale;
+in
 {
   services.tailscale = {
     enable = true;
     openFirewall = true;
-    useRoutingFeatures = lib.mkIf config.deviceSpecific.isServer "server";
-    extraSetFlags = lib.mkIf config.deviceSpecific.isServer [ "--advertise-exit-node" ];
+    useRoutingFeatures = "both";
+    extraSetFlags = lib.optionals config.deviceSpecific.isServer "--advertise-exit-node";
+    extraUpFlags = [ "--accept-dns=false" ]; # TODO: Use authKeyFile to get this working evverywhere
     permitCertUid = lib.mkIf config.services.caddy.enable config.services.caddy.user;
+  };
+
+  networking = {
+    resolvconf.extraConfig = ''
+      prepend_nameservers=100.100.100.100
+      search_domains=angora-ide.ts.net
+    '';
+
+    networkmanager.unmanaged = with ts; [ interfaceName ];
+    firewall.trustedInterfaces = with ts; [ interfaceName ];
+  };
+
+  systemd.services.tailscaled = {
+    before = [ "network.target" ];
+    after = [
+      "dnscrypt-proxy2.service"
+    ];
   };
 }
