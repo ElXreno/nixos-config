46eb5d36949e325e0a5d97d59359d84ef1f8c11a
ElXreno <elxreno@gmail.com>
2025-06-06 23:27:18 +0300

    KURWA: Let's use second nvme for some trash

--- disko/kurwa.nix
+++ disko/kurwa.nix
@@ -33,6 +33,32 @@ _: {
           };
         };
       };
+      nvme_trashbin = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-eui.0025384641baab15";
+        content = {
+          type = "gpt";
+          partitions = {
+            nvme-trashbin = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/trashbin" = {
+                    mountpoint = "/mnt/trashbin";
+                    mountOptions = [ "compress-force=zstd" ];
+                  };
+                  "/android" = {
+                    mountpoint = "/mnt/android";
+                    mountOptions = [ "compress-force=zstd" ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
