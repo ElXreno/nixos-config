a857514e5bc463c55c0a569d24e1ebd6547f16a8
ElXreno <elxreno@gmail.com>
2025-07-04 10:05:50 +0300

    dnscrypt-proxy2: Use custom bootstrap resolvers & listen on dualstack

--- profiles/services/dnscrypt-proxy2.nix
+++ profiles/services/dnscrypt-proxy2.nix
@@ -4,6 +4,16 @@ _: {
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
+      listen_addresses = [
+        "127.0.0.1:53"
+        "[::1]:53"
+      ];
+
+      bootstrap_resolvers = [
+        "1.1.1.1:53"
+        "8.8.8.8:53"
+      ];
+
       http3 = true;
       http3_probe = true;
 
@@ -26,18 +36,6 @@ _: {
             "https://download.dnscrypt.net/resolvers-list/v3/public-resolvers.md"
           ];
           cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
-          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-          refresh_delay = 72;
-        };
-
-        relays = {
-          urls = [
-            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
-            "https://download.dnscrypt.info/resolvers-list/v3/relays.md"
-            "https://ipv6.download.dnscrypt.info/resolvers-list/v3/relays.md"
-            "https://download.dnscrypt.net/resolvers-list/v3/relays.md"
-          ];
-          cache_file = "/var/lib/dnscrypt-proxy/relays.md";
           minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
           refresh_delay = 72;
         };
