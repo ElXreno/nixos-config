7ec819f410c3165509b4116b1384fbe0b857ef19
ElXreno <elxreno@gmail.com>
2025-07-15 23:36:22 +0300

    mpv: Use modern `vo`

--- profiles/home/programs/mpv.nix
+++ profiles/home/programs/mpv.nix
@@ -5,7 +5,8 @@
       ytdl-format = "bestvideo[height<=1080]+(bestaudio[acodec=opus]/bestaudio)/best[height<=1080]";
       hwdec = "auto";
       hwdec-codecs = "all";
-      vo = "gpu";
+      gpu-context = "wayland";
+      vo = "dmabuf-wayland";
       save-position-on-quit = "yes";
       hr-seek = "yes";
       demuxer-max-bytes = "128M";
