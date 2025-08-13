ae39be5747bf12f279ac5d4264b7d116cfe16e02
ElXreno <elxreno@gmail.com>
2025-08-13 22:03:01 +0300

    KURWA: Drop monitoring (I don't need it anymore)

--- machines/KURWA/default.nix
+++ machines/KURWA/default.nix
@@ -25,7 +25,6 @@
     inputs.self.nixosProfiles.services.asusd
     inputs.self.nixosProfiles.services.supergfxd
     inputs.self.nixosProfiles.services.tailscale
-    inputs.self.nixosProfiles.services.monitoring
     inputs.self.nixosProfiles.services.sing-box-client
     inputs.self.nixosProfiles.kde
     inputs.self.nixosProfiles.zfs
