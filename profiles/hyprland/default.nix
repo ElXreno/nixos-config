0dbeb23359c43d91ee61b6d431f7f34a26558d5d
ElXreno <elxreno@gmail.com>
2025-09-06 22:58:57 +0300

    hyprland: Disable ANR dialog

--- profiles/hyprland/default.nix
+++ profiles/hyprland/default.nix
@@ -321,6 +321,7 @@ in
           disable_hyprland_logo = true;
           disable_splash_rendering = true;
           force_default_wallpaper = 0;
+          enable_anr_dialog = false;
         };
 
         exec-once = [
