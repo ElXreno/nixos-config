3b704142a728d3a64e94f5f589432300cfb3a7a6
ElXreno <elxreno@gmail.com>
2025-09-18 16:26:04 +0300

    postgresql: Add upgrade script

--- profiles/services/postgresql.nix
+++ profiles/services/postgresql.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 {
   services.postgresql = {
     enable = true;
@@ -22,4 +22,35 @@
       commit_siblings = 5;
     };
   };
+
+  environment.systemPackages = [
+    (
+      let
+        newPostgres = pkgs.postgresql_17.withPackages (pp: [
+          # pp.plv8
+        ]);
+        cfg = config.services.postgresql;
+      in
+      pkgs.writeScriptBin "upgrade-pg-cluster" ''
+        set -eux
+        # XXX it's perhaps advisable to stop all services that depend on postgresql
+        systemctl stop postgresql
+
+        export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+        export NEWBIN="${newPostgres}/bin"
+
+        export OLDDATA="${cfg.dataDir}"
+        export OLDBIN="${cfg.finalPackage}/bin"
+
+        install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+        cd "$NEWDATA"
+        sudo -u postgres "$NEWBIN/initdb" -D "$NEWDATA" ${lib.escapeShellArgs cfg.initdbArgs}
+
+        sudo -u postgres "$NEWBIN/pg_upgrade" \
+          --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+          --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
+          "$@"
+      ''
+    )
+  ];
 }
