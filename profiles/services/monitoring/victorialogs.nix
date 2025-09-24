1c877e6c031b9bd9386eda591a733aba561d1ef5
ElXreno <elxreno@gmail.com>
2025-09-25 02:51:24 +0300

    victorialogs: Random netflow logs processing

--- profiles/services/monitoring/victorialogs.nix
+++ profiles/services/monitoring/victorialogs.nix
@@ -1,4 +1,4 @@
-_:
+{ pkgs, ... }:
 
 {
   services.victorialogs = {
@@ -6,6 +6,51 @@ _:
     extraOptions = [
       "-retentionPeriod=14d"
     ];
+  };
+
+  environment.systemPackages = [ pkgs.goflow2 ];
+  networking.firewall.allowedUDPPorts = [ 2055 ];
+
+  services.vector = {
+    enable = true;
+    settings = {
+      sources = {
+        netflow_file = {
+          type = "file";
+          include = [ "/var/log/netflow.jsonl" ];
+          read_from = "beginning";
+          ignore_checkpoints = true;
+        };
+      };
+
+      transforms.parse_netflow = {
+        type = "remap";
+        inputs = [ "netflow_file" ];
+        source = ''
+          .message = string!(.message)
+          parsed, err = parse_json(.message)
+          if err == null && is_object(parsed) {
+            . = merge!(., parsed)
+          }
+        '';
+      };
+
+      sinks = {
+        vlogs = {
+          type = "elasticsearch";
+          inputs = [ "parse_netflow" ];
+          endpoints = [ "http://localhost:9428/insert/elasticsearch/" ];
+          api_version = "v8";
+          compression = "gzip";
+          healthcheck.enabled = false;
+          query = {
+            _msg_field = "message";
+            _time_field = "time_received_ns";
+            _stream_fields = "src_addr,src_port,dst_addr,dst_port,proto";
+          };
+        };
+      };
+    };
   };
 
   services.journald.upload = {
