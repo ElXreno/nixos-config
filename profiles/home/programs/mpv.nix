2d1f957bc440549343277a5aaedae939c3233908
ElXreno <elxreno@gmail.com>
2025-09-25 02:51:03 +0300

    mpv: Switch to gpu-next & use nvdec-copy conditionally

--- profiles/home/programs/mpv.nix
+++ profiles/home/programs/mpv.nix
@@ -1,12 +1,13 @@
+{ config, ... }:
 {
   home-manager.users.elxreno.programs.mpv = {
     enable = true;
     config = {
       ytdl-format = "bestvideo[height<=1080]+(bestaudio[acodec=opus]/bestaudio)/best[height<=1080]";
-      hwdec = "auto";
+      hwdec = if config.hardware.nvidia.enabled then "nvdec-copy" else "auto";
       hwdec-codecs = "all";
       gpu-context = "wayland";
-      vo = "dmabuf-wayland";
+      vo = "gpu-next";
       save-position-on-quit = "yes";
       hr-seek = "yes";
       demuxer-max-bytes = "128M";
