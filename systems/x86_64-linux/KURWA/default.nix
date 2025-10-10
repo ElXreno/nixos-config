bcc8a3f7aa3681454bcf9f9ac8824173af8af9f5
ElXreno <elxreno@gmail.com>
2025-10-10 15:39:38 +0300

    systems/KURWA: Disable scx

--- systems/x86_64-linux/KURWA/default.nix
+++ systems/x86_64-linux/KURWA/default.nix
@@ -54,7 +54,8 @@
       supergfxd.enable = true;
 
       sing-box.client.enable = true;
-      scx.enable = true;
+      # Slow downs network I/O when CPU is busy
+      # scx.enable = true;
       postgresql.enable = true;
       bitmagnet.enable = true;
       restic.enable = !virtual;
