b3e85b77ab9e6540fd76fd9a18998cbde578ee26
ElXreno <elxreno@gmail.com>
2025-10-16 21:58:57 +0300

    modules/nixos/services/xray/server: Route IPv6 traffic via cloudflare

--- modules/nixos/services/xray/server/default.nix
+++ modules/nixos/services/xray/server/default.nix
@@ -60,6 +60,7 @@ in
                 ip = [
                   "geoip:by"
                   "geoip:ru"
+                  "::/0"
                 ];
                 outboundTag = "cloudflare";
               }
