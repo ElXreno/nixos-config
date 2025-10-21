1a54dc5a7d05f660052b3da1501e71edf688a4c7
ElXreno <elxreno@gmail.com>
2025-10-21 05:21:19 +0300

    homes/elxreno@KURWA: Setup syncthing

--- homes/x86_64-linux/elxreno@KURWA/default.nix
+++ homes/x86_64-linux/elxreno@KURWA/default.nix
@@ -17,7 +17,36 @@
 
     services = {
       gpg-agent.enable = true;
-      syncthing.enable = true;
+      syncthing = {
+        enable = true;
+        settings = {
+          devices = {
+            SM-938B.id = "54NW2KX-ZL5JFVK-TSF6GXD-W6ZAQCN-VZS3DAJ-EKRLKJG-AX6W6AH-VQPBWA2";
+          };
+          folders = {
+            "~/Sync/Books" = {
+              id = "mb33z-6aqm9";
+              devices = [ "SM-938B" ];
+            };
+            "~/Sync/Camera" = {
+              id = "29llw-v8myj";
+              devices = [ "SM-938B" ];
+            };
+            "~/Sync/Pictures" = {
+              id = "s1iqf-q30lv";
+              devices = [ "SM-938B" ];
+            };
+            "~/Sync/Music" = {
+              id = "cnd20-w7p5o";
+              devices = [ "SM-938B" ];
+            };
+            "~/Sync/RandomStuff" = {
+              id = "ywnqs-d6y2m";
+              devices = [ "SM-938B" ];
+            };
+          };
+        };
+      };
     };
   };
 
