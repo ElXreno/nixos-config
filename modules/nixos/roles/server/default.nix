2f32dacccaf85b8bc659c23e0f4f2594321c8f41
ElXreno <elxreno@gmail.com>
2025-10-21 06:07:38 +0300

    modules/nixos/roles/server: Enable auto upgrade by default

--- modules/nixos/roles/server/default.nix
+++ modules/nixos/roles/server/default.nix
@@ -23,10 +23,16 @@ in
       };
 
       system = {
+        autoupgrade = {
+          enable = true;
+          allowReboot = true;
+        };
+
         nix = {
           auto-optimise.enable = true;
           gc.enable = true;
         };
+
         documentation.enable = false;
       };
     };
