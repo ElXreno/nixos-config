07462b0c3644566caea468ed562e6ea5db34bfcf
ElXreno <elxreno@gmail.com>
2025-10-27 13:02:33 +0300

    modules/nixos/services/pipewire: Add some additional real-time audio processing possibilities

--- modules/nixos/services/pipewire/default.nix
+++ modules/nixos/services/pipewire/default.nix
@@ -83,5 +83,41 @@ in
         };
       };
     };
+
+    # Real-time audio processing
+    security.pam.loginLimits = [
+      {
+        domain = "@audio";
+        item = "memlock";
+        type = "-";
+        value = "unlimited";
+      }
+      {
+        domain = "@audio";
+        item = "rtprio";
+        type = "-";
+        value = "99";
+      }
+      {
+        domain = "@audio";
+        item = "nofile";
+        type = "soft";
+        value = "99999";
+      }
+      {
+        domain = "@audio";
+        item = "nofile";
+        type = "hard";
+        value = "99999";
+      }
+    ];
+
+    services.udev = {
+      extraRules = ''
+        KERNEL=="rtc0", GROUP="audio"
+        KERNEL=="hpet", GROUP="audio"
+        DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+      '';
+    };
   };
 }
