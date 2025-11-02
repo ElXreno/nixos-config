d42cf2c2e0d71d972ad60b96622aad0c426fa795
ElXreno <elxreno@gmail.com>
2025-11-02 20:02:42 +0300

    modules/nixos/services/flatpak: More stuff

--- modules/nixos/services/flatpak/default.nix
+++ modules/nixos/services/flatpak/default.nix
@@ -18,9 +18,24 @@ in
   config = mkIf cfg.enable {
     services.flatpak.enable = true;
     environment.systemPackages = with pkgs; [
-      appstream
       flatpak-builder
+
+      appstream
       desktop-file-utils
+      ostree-full
+      gdk-pixbuf
+      librsvg
     ];
+
+    environment.pathsToLink = [
+      "share/thumbnailers"
+    ]; # TODO: Move to hyprland
+
+    environment.sessionVariables = {
+      # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load
+      # SVG files (important for icons)
+      # Fixes error: .../share/icons/hicolor/scalable/apps/blabla.svg is not a valid icon: Format not recognized
+      GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
+    };
   };
 }
