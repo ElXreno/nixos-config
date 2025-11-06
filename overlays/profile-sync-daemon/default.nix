8bb7fa86eb149edf4713fe7af5beaaea2b086a3c
ElXreno <elxreno@gmail.com>
2025-11-06 10:27:28 +0300

    overlays/profile-sync-daemon: Bump to latest commit
    Most wanted fix:
    https://github.com/graysky2/profile-sync-daemon/commit/9491a03303f85ed31d781e962d82583ade77cacc

---
+++ overlays/profile-sync-daemon/default.nix
@@ -0,0 +1,16 @@
+_:
+
+_final: prev: {
+  profile-sync-daemon = prev.profile-sync-daemon.overrideAttrs (
+    _finalAttrs: prevAttrs: {
+      version = "unstable-2025-07-25";
+
+      src = prev.fetchFromGitHub {
+        owner = "graysky2";
+        repo = "profile-sync-daemon";
+        rev = "bdea6f002018bd23e8734e87974553835b1d45e9";
+        hash = "sha256-GIj2AjTWo9M3k/5R3FnYQBpHvXiIrF+1gPVHUGb6mp4=";
+      };
+    }
+  );
+}
