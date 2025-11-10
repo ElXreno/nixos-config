e45b5486664fab973ef62ecdcbb47a1af3c77e9f
ElXreno <elxreno@gmail.com>
2025-11-10 08:18:18 +0300

    homes/elxreno@GRATE: htop: Force enable advanced cpu stats

--- homes/x86_64-linux/elxreno@GRATE/default.nix
+++ homes/x86_64-linux/elxreno@GRATE/default.nix
@@ -1,9 +1,9 @@
 { namespace, ... }:
 {
   ${namespace} = {
-    roles = {
-      server.enable = true;
-    };
+    roles.server.enable = true;
+
+    programs.htop.showAdvancedCPUStats = true;
   };
 
   home.stateVersion = "25.05";
