a5a1d68e82ac3ee8839ddd792117d429337028af
ElXreno <elxreno@gmail.com>
2025-12-04 06:05:42 +0300

    packages/headlamp: 0.31.1 -> 0.38.0

--- packages/headlamp/default.nix
+++ packages/headlamp/default.nix
@@ -1,12 +1,12 @@
 { pkgs, fetchurl, ... }:
 pkgs.appimageTools.wrapType2 rec {
   pname = "headlamp";
-  version = "0.31.1";
+  version = "0.38.0";
 
   src = fetchurl {
     url = "https://github.com/kubernetes-sigs/headlamp/releases/download/v${version}/Headlamp-${version}-linux-x64.AppImage";
     name = "${pname}-${version}.AppImage";
-    hash = "sha256-Ze6c5IImWbPyBivwavy7IRrVD40HLr8UAJb69uMKu/A=";
+    hash = "sha256-WIsr/lG+yAyoKITk7kka9HEw5nr0o2f4rKcIVf+trP4=";
   };
 
   extraInstallCommands =
@@ -14,10 +14,9 @@ pkgs.appimageTools.wrapType2 rec {
       contents = pkgs.appimageTools.extractType1 { inherit pname src version; };
     in
     ''
-      mkdir -p "$out/share/applications"
-      mkdir -p "$out/share/lib/headlamp"
-      cp -r ${contents}/* "$out/share/lib/headlamp"
-      cp "${contents}/${pname}.desktop" "$out/share/applications/"
+      mkdir -p $out/share/{applications,lib/headlamp}
+      cp -r ${contents}/* $out/share/lib/headlamp
+      cp ${contents}/${pname}.desktop $out/share/applications/
       substituteInPlace $out/share/applications/${pname}.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
     '';
 }
