2eded0e0595f3236cc3ac0ffcedb447d6a986bb9
ElXreno <elxreno@gmail.com>
2025-12-13 11:19:04 +0300

    packages/flatpak-cpan-generator: Init

---
+++ packages/flatpak-cpan-generator/default.nix
@@ -0,0 +1,45 @@
+{
+  buildFHSEnv,
+  fetchFromGitHub,
+  perl,
+  openssl,
+  gnumake,
+  pkg-config,
+  gcc,
+  libxcrypt,
+}:
+let
+  customPerl = perl.withPackages (
+    p: with p; [
+      CaptureTiny
+      GetoptLongDescriptive
+      JSONMaybeXS
+      LWP
+      LWPProtocolHttps
+      MetaCPANClient
+      Appcpanminus
+    ]
+  );
+
+  src = fetchFromGitHub {
+    owner = "flatpak";
+    repo = "flatpak-builder-tools";
+    rev = "fdbe66a48b7450f3c18ab2cb8ff31be704846600";
+    hash = "sha256-LpzAIylwDi/R0CO21JHMNjSG9MBMK11h8TIW/eL5UaU=";
+  };
+in
+buildFHSEnv {
+  name = "flatpak-cpan-generator";
+
+  targetPkgs = pkgs: [
+    customPerl
+    openssl
+    openssl.dev
+    gnumake
+    libxcrypt
+    pkg-config
+    gcc
+  ];
+
+  runScript = "${src}/cpan/flatpak-cpan-generator.pl";
+}
