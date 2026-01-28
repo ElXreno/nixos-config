decf06d6452e1aa613be4b53cf960e95afb957fc
ElXreno <elxreno@gmail.com>
2026-01-28 20:23:03 +0300

    modules/home/desktop-environments/niri: Set _JAVA_AWT_WM_NONREPARENTING

--- modules/home/desktop-environments/niri/default.nix
+++ modules/home/desktop-environments/niri/default.nix
@@ -59,6 +59,8 @@ in
     };
 
     home.sessionVariables = {
+      _JAVA_AWT_WM_NONREPARENTING = 1;
+
       QT_QPA_PLATFORM = "wayland;xcb";
       CLUTTER_BACKEND = "wayland";
       SDL_VIDEODRIVER = "wayland";
