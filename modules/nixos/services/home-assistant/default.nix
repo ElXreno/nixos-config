73a58b260afc6a1a3311850b8748eb6d45a5a7ca
ElXreno <elxreno@gmail.com>
2026-04-23 00:13:09 +0300

    feat(hass): expose over CF

--- modules/nixos/services/home-assistant/default.nix
+++ modules/nixos/services/home-assistant/default.nix
@@ -7,8 +7,30 @@
 }:
 
 let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    concatMapStringsSep
+    splitString
+    filter
+    ;
   cfg = config.${namespace}.services.home-assistant;
+
+  cloudflareIpRanges = filter (s: s != "") (
+    splitString "\n" (
+      builtins.readFile (
+        pkgs.fetchurl {
+          url = "https://www.cloudflare.com/ips-v4";
+          hash = "sha256-8Cxtg7wBqwroV3Fg4DbXAMdFU1m84FTfiE5dfZ5Onns=";
+        }
+      )
+    )
+  );
+
+  realIpFromCloudflare = concatMapStringsSep "\n" (
+    cidr: "set_real_ip_from ${cidr};"
+  ) cloudflareIpRanges;
+  cfAllowedGeoEntries = concatMapStringsSep "\n" (cidr: "${cidr} 1;") cloudflareIpRanges;
 in
 {
   options.${namespace}.services.home-assistant = {
@@ -23,11 +45,49 @@ in
       services = {
         zigbee2mqtt.enable = cfg.zigbee2mqtt.enable;
         postgresql.enable = true;
+        nginx = {
+          enable = true;
+          virtualHosts."hass.elxreno.com" = {
+            acmeRoot = null;
+            extraConfig = ''
+              ${realIpFromCloudflare}
+              real_ip_header CF-Connecting-IP;
+              if ($cf_allowed = 0) { return 403; }
+            '';
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:8123";
+              proxyWebsockets = true;
+            };
+          };
+        };
       };
       tailscale.serve."svc:hass".endpoints."tcp:443" = "http://localhost:8123";
     };
 
     networking.firewall.allowedTCPPorts = [ 8123 ];
+
+    services.nginx.appendHttpConfig = ''
+      geo $realip_remote_addr $cf_allowed {
+        default 0;
+        127.0.0.1 1;
+        ${cfAllowedGeoEntries}
+      }
+    '';
+
+    security.acme.certs."hass.elxreno.com" = {
+      dnsProvider = "cloudflare";
+      extraLegoFlags = [ "--dns.propagation-wait=60s" ];
+      credentialFiles.CLOUDFLARE_DNS_API_TOKEN_FILE =
+        config.clan.core.vars.generators.acme-cloudflare.files.token.path;
+    };
+
+    clan.core.vars.generators.acme-cloudflare = {
+      prompts.token = {
+        description = "Cloudflare API token (Zone:Zone:Read + Zone:DNS:Edit)";
+        type = "hidden";
+        persist = true;
+      };
+    };
 
     systemd.tmpfiles.rules = [
       "f /var/lib/hass/automations.yaml 0644 hass hass - []"
@@ -95,7 +155,7 @@ in
       config = {
         # https://www.home-assistant.io/integrations/default_config/
         default_config = { };
-        homeassistant = { };
+        homeassistant.external_url = "https://hass.elxreno.com";
 
         automation = "!include automations.yaml";
         script = "!include scripts.yaml";
