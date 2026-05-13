dba0e0b4ee15801fe5213cffd78cc86a248730fa
ElXreno <elxreno@gmail.com>
2026-05-14 01:31:20 +0300

    build(deps): update flake inputs

--- overlays/tailscale/default.nix
+++ overlays/tailscale/default.nix
@@ -6,6 +6,11 @@ _final: prev: {
       postPatch = (prevAttrs.postPatch or "") + ''
         substituteInPlace wgengine/magicsock/magicsock.go \
           --replace-fail 'socketBufferSize = 7 << 20' 'socketBufferSize = 16 << 20'
+
+        # https://github.com/tailscale/tailscale/issues/19730
+        substituteInPlace net/dns/dns_clone.go \
+          --replace-fail $'for k, sv := range src.Routes {\n\t\t\tif sv == nil {\n\t\t\t\tcontinue\n\t\t\t}' \
+                         $'for k, sv := range src.Routes {\n\t\t\tif sv == nil {\n\t\t\t\tdst.Routes[k] = nil\n\t\t\t\tcontinue\n\t\t\t}'
       '';
     }
   );
