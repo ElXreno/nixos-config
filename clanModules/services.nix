2099b7f98997c8d69142e5225d4c795d4dc1fb03
ElXreno <elxreno@gmail.com>
2026-05-26 07:48:09 +0300

    feat(ncps): add cache.nixos-cuda.org

--- clanModules/services.nix
+++ clanModules/services.nix
@@ -58,6 +58,7 @@ in
         machines.BIMBA.settings = {
           caches = [
             "https://nixos-cache-proxy.elxreno.com"
+            "https://cache.nixos-cuda.org"
             "https://nix-community.cachix.org"
             "https://nixpkgs-unfree.cachix.org"
             "https://nix-gaming.cachix.org"
@@ -67,6 +68,7 @@ in
 
           publicKeys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
             "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
