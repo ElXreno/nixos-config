4edb2b0c692e10973d51eb7025d982de2a47df0f
ElXreno <elxreno@gmail.com>
2026-06-13 14:24:57 +0300

    feat(ncps): numtide cache

--- clanModules/services.nix
+++ clanModules/services.nix
@@ -85,6 +85,7 @@ in
             "https://nix-gaming.cachix.org"
             "https://cuda-maintainers.cachix.org"
             "https://niri.cachix.org"
+            "https://cache.numtide.com"
           ];
 
           publicKeys = [
@@ -95,6 +96,7 @@ in
             "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
             "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
             "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+            "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
           ];
         };
         extraModules = [
